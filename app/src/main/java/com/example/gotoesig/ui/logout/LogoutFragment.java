676b0cc634de3ec0b89eda12687258fd2b688680
gprize <ngom.p.william@gmail.com>
2024-12-12 00:30:53 +0100

    refacto logoutFragment

--- app/src/main/java/com/example/gotoesig/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/gotoesig/ui/logout/LogoutFragment.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.gotoesig.AuthActivity;
 import com.example.gotoesig.databinding.FragmentLogoutBinding;
@@ -19,42 +16,26 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LogoutFragment extends Fragment {
 
     private FragmentLogoutBinding binding;
-    private FirebaseAuth auth;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        // Initialisation du ViewModel (si nécessaire)
-        LogoutViewModel slideshowViewModel = new ViewModelProvider(this).get(LogoutViewModel.class);
-
-        // Récupération du binding pour lier le layout XML
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentLogoutBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        // Initialisation de Firebase Auth
-        auth = FirebaseAuth.getInstance();
+        // Déconnexion lorsque le bouton est cliqué
+        binding.btnLogout.setOnClickListener(v -> logoutUser());
 
-        // Ajouter un bouton pour gérer la déconnexion
-        Button btnLogout = binding.btnLogout;
-        btnLogout.setOnClickListener(v -> logoutUser());
-
-        return root;
+        return binding.getRoot();
     }
 
-    // Méthode pour déconnecter l'utilisateur et revenir à l'écran de connexion
     private void logoutUser() {
         // Déconnexion de Firebase
-        auth.signOut();
+        FirebaseAuth.getInstance().signOut();
 
-        // Fermer toutes les activités
-        if (getActivity() != null) {
-            getActivity().finishAffinity(); // Ferme toutes les activités
-
-            // Lancer l'activité d'authentification
-            Intent intent = new Intent(getActivity(), AuthActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Assure que l'AuthActivity soit la seule activité ouverte
-            startActivity(intent);
-        }
+        // Retour à l'activité d'authentification
+        Intent intent = new Intent(requireContext(), AuthActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        requireActivity().finish();
     }
 
     @Override
